be679f733dc773008e6a12c1cfead910cbb39047
Lina Brunken <linatt@hotmail.de>
2018-06-04 01:38:16 +0200

    test changes

---
+++ MicroserviceRequ/src/main/java/test/ReqTest.java
@@ -0,0 +1,118 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ReqTest {
+
+    @Test
+        public void ServertestLina(){
+            try{
+                String firstname = "lina";
+                String lastname = "brunken";
+                String url = "http://localhost:8001/name?firstname=" + firstname + "&lastname=" + lastname;
+                //reg name
+                URL obj = new URL(url);
+
+                System.out.print("\nSending GET-Request: " + url + "\n");
+                //
+                //build connection
+                HttpURLConnection httpcon = (HttpURLConnection) obj.openConnection();
+
+                int responseCode = httpcon.getResponseCode();
+
+                Assert.assertEquals(200, responseCode);
+
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(httpcon.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                Assert.assertEquals("Hallo Lina!", response.toString());
+
+
+            }catch (Exception e){
+                System.out.print(e + "\n");
+            }
+    }
+    @Test
+    public void ServertestUnknown(){
+        try{
+            String firstname = "unknown";
+            String lastname = "lol";
+            String url = "http://localhost:8001/name?firstname=" + firstname + "&lastname=" + lastname;
+            //reg name
+            URL obj = new URL(url);
+
+            System.out.print("\nSending GET-Request: " + url + "\n");
+            //
+            //build connection
+            HttpURLConnection httpcon = (HttpURLConnection) obj.openConnection();
+
+            int responseCode = httpcon.getResponseCode();
+
+            Assert.assertEquals(200, responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(httpcon.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            Assert.assertEquals("Hallo Unbekannt!", response.toString());
+
+
+        }catch (Exception e){
+            System.out.print(e + "\n");
+        }
+
+
+    }
+    @Test
+    public void ServertestUnknown2(){
+        try{
+            String firstname = "";
+            String lastname = "lol";
+            String url = "http://localhost:8001/name?firstname=" + firstname + "&lastname=" + lastname;
+            //reg name
+            URL obj = new URL(url);
+
+            System.out.print("\nSending GET-Request: " + url + "\n");
+            //
+            //build connection
+            HttpURLConnection httpcon = (HttpURLConnection) obj.openConnection();
+
+            int responseCode = httpcon.getResponseCode();
+
+            Assert.assertEquals(200, responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(httpcon.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            Assert.assertEquals("Hallo Unbekannt!", response.toString());
+
+
+        }catch (Exception e){
+            System.out.print(e + "\n");
+        }
+
+
+    }
+
+
+}
