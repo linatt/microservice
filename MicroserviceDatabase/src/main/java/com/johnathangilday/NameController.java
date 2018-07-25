5a747386c3d31a0d4ab8fb0ec626182f11c697c6
Lina Brunken <linatt@hotmail.de>
2018-07-25 15:09:19 +0200

    25-07-2018

--- MicroserviceDatabase/src/main/java/com/johnathangilday/NameController.java
+++ MicroserviceDatabase/src/main/java/com/johnathangilday/NameController.java
@@ -8,27 +8,50 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.ParamConverter;
 import java.net.UnknownHostException;
 
-@Path("/name")
-@Produces(MediaType.TEXT_PLAIN)
-public class NameController {
+//@Path("/name")
+//@Produces(MediaType.TEXT_PLAIN)
+//public class NameController {
 
-    @POST
     @Path("/register")
-    public String register(@FormParam("name") String name, @FormParam("url") String url) throws UnknownHostException {
-   // public String register (@QueryParam("name") String name, @QueryParam("url") String url) throws UnknownHostException {
-       // public String register(@PathParam("name") final String name, @PathParam("url") final String url) throws UnknownHostException {
-        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
-        DB registry = mongoClient.getDB("registry");
+    public class NameController {
 
-        DBCollection services = registry.getCollection("services");
-        DBObject query = new BasicDBObject();
-        query.put("name", name);
-        DBObject update = new BasicDBObject();
-        update.put("name", name);
-        update.put("url", url);
-        DBObject res = services.findAndModify(query, new BasicDBObject(), new BasicDBObject(), false, update, true, true);
-        return "cool";
-    }
+//        @GET
+//        @Produces(MediaType.TEXT_PLAIN)
+//        public String register(@QueryParam("name") String name, @QueryParam("url") String url)  {
+//            MongoClient mongoClient = null;
+//            try {
+//                mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+//            } catch (UnknownHostException e) {
+//                e.printStackTrace();
+//            }
+//            DB registry = mongoClient.getDB("registry");
+//            DBCollection services = registry.getCollection("services");
+//            DBObject query = new BasicDBObject();
+//            query.put("name", name);
+//            DBObject update = new BasicDBObject();
+//            update.put("name", name);
+//            update.put("url", url);
+//            DBObject res = services.findAndModify(query, new BasicDBObject(), new BasicDBObject(), false, update, true, true);
+//            return "cool";
+//        }
+
+//    @POST
+//    @Path("/register")
+//    public String register(@FormParam("name") String name, @FormParam("url") String url) throws UnknownHostException {
+//   // public String register (@QueryParam("name") String name, @QueryParam("url") String url) throws UnknownHostException {
+//       // public String register(@PathParam("name") final String name, @PathParam("url") final String url) throws UnknownHostException {
+//        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+//        DB registry = mongoClient.getDB("registry");
+//
+//        DBCollection services = registry.getCollection("services");
+//        DBObject query = new BasicDBObject();
+//        query.put("name", name);
+//        DBObject update = new BasicDBObject();
+//        update.put("name", name);
+//        update.put("url", url);
+//        DBObject res = services.findAndModify(query, new BasicDBObject(), new BasicDBObject(), false, update, true, true);
+//        return "OK";
+//    }
 
     @GET
     @Path("/{name}")
